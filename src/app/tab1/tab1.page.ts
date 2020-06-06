5a234856adc168878600cfe635b3570ed050b529
Carlos Eduardo Artioli <caduartioli@gmail.com>
2020-06-06 19:58:27 -0300

    Criação da página Calculadora Simples

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  public numero1: any;
+  public numero2: any;
+  public resultado: any;
+
   constructor() {}
+
+  public limpar(){
+
+    this.numero1 = "";
+    this.numero2 = "";
+    this.resultado = "";
+
+  }
+
+  public somar(){
+
+    this.resultado = this.numero1 + this.numero2
+
+  }
+
+  public subtrair(){
+
+    this.resultado = this.numero1 - this.numero2
+    
+  }
+
+  public dividir(){
+
+    this.resultado = this.numero1 / this.numero2
+    
+  }
+
+  public multiplicar(){
+
+    this.resultado = this.numero1 * this.numero2
+    
+  }
 
 }
